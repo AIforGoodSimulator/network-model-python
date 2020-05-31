f6c95564670b9dd00eb78470f321eb57f5000325
Marcelo Sandoval-Castaneda <jms1595@nyu.edu>
2020-05-31 17:46:59 +0400

    Added functions to calculate the probability of being symptomatic

--- stats_utils.py
+++ stats_utils.py
@@ -29,6 +29,16 @@ def get_deathrate(row):
         return deathrate_female(row["age"])
 
 
+def sympt_prob(x):
+    return 0.41731169 + -1.38217963e-02*x**1 + 5.19931131e-04*x**2 + -3.66388844e-06*x**3
+
+
+def get_prob_symptomatic(row):
+    if row["age"] > 79:
+        return sympt_prob(79)
+    return sympt_prob(row["age"])
+
+
 def sample_population(n_sample):
     # Dataframe including age (V1) and sex (V2)
     pop_df = pd.read_csv('data/age_and_sex.csv')
@@ -36,6 +46,7 @@ def sample_population(n_sample):
 
     # Sample only the number of people in isoboxes (n_samples)
     pop_df['death_rate'] = pop_df.apply(lambda row: get_deathrate(row), axis=1)
+    pop_df['prob_symptomatic'] = pop_df.apply(lambda row: get_prob_symptomatic(row), axis=1)
     sample = pop_df.sample(n=n_sample, random_state=69420)
     # print(sample['death_rate'].values.shape)
 
