3245c3fecf594d51d94a045b072f641d51d546bf
thefirebanks <dfireban@oberlin.edu>
2020-06-02 12:33:19 -0700

    added more network utils methods

--- network_utils.py
+++ network_utils.py
@@ -3,12 +3,16 @@ import networkx as nx
 import itertools
 from tqdm import tqdm
 # from seirsplus.models import *
-# import pandas as pd
+import pandas as pd
 import numpy as np
-# import matplotlib.pyplot as plt
+from collections import defaultdict
+
+STATE_DICTIONARY = {"Susceptible": 1, "Exposed": 2, "Infectious": 3,
+                     "Detected_Exposed": 4, "Detected_Infectious": 5,
+                     "Recovered": 6, "Deceased": 7}
 
 
-def create_graph(n_structures, population, max_pop_per_struct, edge_weight):
+def create_graph(n_structures, population, max_pop_per_struct, edge_weight, label):
     """ Creates a networkX graph containing all the population in the camp that is in a given structure (currently just isoboxes).
         Draws edges between people from the same isobox and returns the networkX graph and an adjacency list
     """
@@ -49,14 +53,53 @@ def create_graph(n_structures, population, max_pop_per_struct, edge_weight):
         # Use the cartesian product to get all possible edges within the nodes in an isobox
         # and only add if they are not the same node
         edge_list = [tup for tup in list(itertools.product(node_list, repeat=2)) if tup[0] != tup[1]]
-        g.add_edges_from(edge_list, weight=edge_weight)
+        g.add_edges_from(edge_list, weight=edge_weight, label=label)
 
     return g, nodes_per_struct
 
 
+def create_distancing_graph(base_graph, scale=20, min_num_edges=8):
+    return remove_edges_from_graph(base_graph, ["food"], scale, min_num_edges)
+
+
+def create_quarantine_graph(base_graph, scale=10, min_num_edges=4):
+    return remove_edges_from_graph(base_graph, ["food", "neighbor"], scale, min_num_edges)
+
+
+def remove_edges_from_graph(base_graph, edge_label_list, scale, min_num_edges):
+    """ Randomly remove some of the edges that have a label included in the label list (i.e 'food', 'neighbors', etc)
+        The scale is a parameter for the exponential distribution and the min_num_edges the minimum number of edges
+        a node should keep (this will be the peak of the distribution) """
+
+    graph = base_graph.copy()
+
+    for node in graph:
+        # Select all the neighbors that share an edge of a particular label
+        neighbors = [neighbor for neighbor in list(graph[node].keys())
+                     if graph.edges[node, neighbor]["label"] in edge_label_list]
+
+        # Randomly draw the number of edges to keep
+        quarantine_edge_num = int(max(min(np.random.exponential(scale=scale, size=1), len(neighbors)), min_num_edges))
+
+        # Create the list of neighbors to keep
+        quarantine_keep_neighbors = np.random.choice(neighbors, size=quarantine_edge_num, replace=False)
+
+        # Remove edges that are not in te list of neighbors to keep
+        for neighbor in neighbors:
+            if neighbor not in quarantine_keep_neighbors:
+                graph.remove_edge(node, neighbor)
+
+    return graph
+
+
+def divide_grid(grid, n_slices):
+    """ Returns a list of evenly sized blocks of the camp grid """
+    return np.array_split(grid, n_slices, axis=1)
+
+
 def create_grid(width, height):
-    """ Create a grid of isoboxes that resembles the isobox area of the camp, for ease of measuring proximity between nodes
-        Returns a numpy array of shape (width, height) """
+    """ Create a grid of isoboxes that resembles the isobox area of the camp, for ease of measuring proximity
+        between nodes. Returns a numpy array of shape (width, height) """
 
     iso_grid = np.zeros(shape=(width, height)).astype(int)
     iso_n = 0
@@ -100,7 +143,7 @@ def get_neighbors(grid, structure_num, proximity):
     return list(neighbors)
 
 
-def connect_neighbors(base_graph, n_structures, nodes_per_structure, grid, proximity, edge_weight):
+def connect_neighbors(base_graph, n_structures, nodes_per_structure, grid, proximity, edge_weight, label):
     """ Draw edges in the given graph between people of neighboring structures (currently isoboxes)
         f they have the same ethnicity """
 
@@ -114,16 +157,16 @@ def connect_neighbors(base_graph, n_structures, nodes_per_structure, grid, proxi
 
         # For every neighbor isobox:
         for neighbor in neighbors:
-
             # If they share the same properties, draw an edge between them
             graph.add_edges_from([(i, j) for i in nodes_per_structure[structure] \
                                   for j in nodes_per_structure[neighbor] if
-                                  graph.nodes[i]["ethnicity"] == graph.nodes[j]["ethnicity"]], weight=edge_weight)
+                                  graph.nodes[i]["ethnicity"] == graph.nodes[j]["ethnicity"]],
+                                 weight=edge_weight, label=label)
 
     return graph
 
 
-def connect_food_queue(base_graph, nodes_per_structure, edge_weight):
+def connect_food_queue(base_graph, nodes_per_structure, edge_weight, label):
     """ Connect 1-2 people per structure (currently just isoboxes) randomly to represent that they go to the food queue
         We have 3 options:
             - Either have a range of people (2-5 per isobox) that go to food queue, same edge weights
@@ -148,14 +191,38 @@ def connect_food_queue(base_graph, nodes_per_structure, edge_weight):
     for i in range(len(food_bois) - 6):
         for j in range(i + 1, i + 6):
             if not graph.has_edge(food_bois[i], food_bois[j]):
-                graph.add_edge(food_bois[i], food_bois[j], weight=edge_weight)
+                graph.add_edge(food_bois[i], food_bois[j], weight=edge_weight, label=label)
     return graph
 
+# TODO: Write this with more information
+def output_df(model, graph, properties, states, store=False):
+    """ Say we wanted to get the number of deaths and infectious individuals per ethnicity:
+        - Properties is a list of ['ethnicity']
+        - States is a list of ['Infectious', 'Deceased']
 
-# Some helper functions
+        Returns a dataframe with the information requested above, and stores it if store=True """
+
+    output = pd.DataFrame()
+    node_groups = defaultdict(list)
+
+    for state in states:
+        # Get all the nodes in a given state - we can access their properties as well
+        nodes = get_nodes_per_state(model, graph, STATE_DICTIONARY[state])
+        node_groups[state] = nodes
+
+
+# Some helper functions - all of which could be added to networkx class!
 def min_degree(graph):
+    """ Return the minimum degree in the graph """
     return min(graph.degree, key=lambda kv: kv[1])[1]
 
 
 def max_degree(graph):
+    """ Return the maximum degree in the graph """
     return max(graph.degree, key=lambda kv: kv[1])[1]
+
+
+def get_nodes_per_state(model, graph, state):
+    """ Get the nodes that have a given state in the latest timestep of the SEIRS+ model
+        Since nodes are represented by their properties in this case, this will return a list of property dicts """
+    return [node for node in graph.nodes if model.X[node][0] == state]
